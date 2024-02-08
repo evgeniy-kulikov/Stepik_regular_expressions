ff0e599d2fc6d0dbf75504e6d7f4ed4d6f2c7103
evgeniy-kulikov <redstepway@gmail.com>
2024-02-08 10:00:33 +0300

    2.10 Группирующие скобки

--- Test_01.py
+++ Test_01.py
@@ -4,9 +4,17 @@ Input:  *
 Output: *
 """
 
-
+# Task 06
 """
 
 Input:  
 Output: 
 """
+import re
+
+# s = input()
+s = 'Я бежал... Пока мышцы не стало жечь огнем, а кровь не стала едкая, как кислота... И тогда... Я побежал дальше...'
+regex = r'\.{3}'
+
+res = re.findall(regex, s)
+print(res)
